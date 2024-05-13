b82f11d1bf2328d184c08cbcac1158a7f62242c0
Neha Rawat <93335677+NehaRawat22@users.noreply.github.com>
2024-05-13 21:20:57 +0530

    Placement Cell Management System

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,114 +1,37 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import back from "../assets/bb.jpg";
+import { FcGoogle } from "react-icons/fc";
+
+import './Login.css';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const [isFormValid, setIsFormValid] = useState(false);
-  const navigate = useNavigate();
+  
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    validateForm();
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    validateForm();
-  };
-
-  const validateForm = () => {
-    const isValid = email.trim() !== "" && password.trim() !== "";
-    setIsFormValid(isValid);
-  };
-
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isFormValid) {
-      console.log("Form submitted successfully");
-      navigate("/dashboard");
-    } else {
-      console.log("Form is invalid. Please fill all fields.");
-    }
-  };
-
-  // Function to toggle password visibility
-  const togglePasswordVisibility = () => {
-    setPasswordVisible((prevState) => !prevState);
-  };
 
   return (
     <div>
-      <section className="bg-gray-50 min-h-screen flex items-center justify-center">
-        <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
-          <div className="md:w-1/2 px-8 md:px-16">
-            <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
+      <div className="login mainer">
+         <img src={back} alt="image" className="login__bg"/>
 
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-              <input
-                className="p-2 mt-8 rounded-xl border"
-                type="email"
-                name="email"
-                placeholder="Email"
-                value={email}
-                onChange={handleEmailChange}
-              />
-              <div className="relative">
-                <input
-                  className="p-2 rounded-xl border w-full"
-                  type={passwordVisible ? "text" : "password"}
-                  name="password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="gray"
-                  className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer"
-                  viewBox="0 0 16 16"
-                  onClick={togglePasswordVisibility}
-                >
-                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                </svg>
-              </div>
-              <button
-                type="submit"
-                className={`bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300 ${
-                  !isFormValid ? "opacity-50 cursor-not-allowed" : ""
-                }`}
-                disabled={!isFormValid}
-              >
-                Login
-              </button>
-            </form>
-            <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-              <hr className="border-gray-400" />
-              <p className="text-center text-sm">OR</p>
-              <hr className="border-gray-400" />
+         <form action="" className="login__form">
+         <h1 className="login__title">LOGIN</h1>
+
+            <div className="login__inputs">
+            <div className="login__box">
+                  <p className="login__input">Login with Google</p>
+                  <FcGoogle style={{fontSize:'40px'}}/>
+                  <i className="ri-lock-2-fill"></i>
+               </div>
             </div>
 
-            <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
-              <p>Don't have an account?</p>
-              <Link to="/register">
-                <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">
-                  Register
-                </button>
-              </Link>
+            <div className="login__register" style={{fontSize:'15px'}}>
+               Welcome! <b>You are a click away </b> to view what's new today
             </div>
-          </div>
-          <div className="md:block hidden w-1/2">
-            <img className="rounded-2xl" src={logo} alt="Logo" />
-          </div>
-        </div>
-      </section>
-    </div>
+         </form>
+         </div>
+         </div>
   );
 };
 
